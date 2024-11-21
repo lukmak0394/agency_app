2df78bca6f0e3169e0816620a5c2006dbcb653f5
lukmak0394 <lukmak0394@wp.pl>
2024-11-22 00:09:47 +0100

    site title

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta charset="UTF-8">
-    <title>Widoczni recruitment-task</title>
+    <title>Agency app</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css"></script>
     <link rel="stylesheet" href="./style.css">
